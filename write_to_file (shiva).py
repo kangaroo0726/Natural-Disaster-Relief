60f52ea7de52ba7d945a2a3f25e8fbf434161d30
Shiva Deshpande <shivadeshpande214@gmail.com>
2025-11-09 11:34:05 -0500

    Store in dictionary

--- write_to_file (shiva).py
+++ write_to_file (shiva).py
@@ -1,29 +1,30 @@
-import json
 import csv
 
-with open("shelterData.csv") as f:
-    data = json.load(f)
-
-with open("shelters.csv", "w", newline="") as f:
-    writer = csv.DictWriter(f, fieldnames=data[0].keys())
-    writer.writeheader()
-    writer.writerows(data)
-
-with open("shelters.csv") as file_in:
-    header = file_in.readline()
-    reader_in = csv.reader(file_in)
-    for line in reader_in:
-        shelter_name = line[0]
-        shelter_address = line[1]
-        shelter_type = line[2]
-        shelter_lat = float(line[3])
-        shelter_lon = float(line[4])
-        shelter_capacity = int(line[5])
-        shelter_occupancy = int(line[6])
-        food_available = True if line[6] == "True" else False
-        water_available = True if line[7] == "True" else False
-        medical_aid_available = True if line[8] == "True" else False
-        pet_friendly = True if line[9] == "True" else False
+def read_csv_to_dict(filename):
+    with open(filename) as file_in:
+        shelter_dict = {}
+        header = file_in.readline()
+        reader_in = csv.reader(file_in)
+        for line in reader_in:
+            shelter_name = line[0]
+            shelter_address = line[1]
+            shelter_type = line[2]
+            shelter_lat = float(line[3])
+            shelter_lon = float(line[4])
+            shelter_capacity = int(line[5])
+            shelter_occupancy = int(line[6])
+            food_available = True if line[6] == "True" else False
+            water_available = True if line[7] == "True" else False
+            medical_aid_available = True if line[8] == "True" else False
+            pet_friendly = True if line[9] == "True" else False
+            shelter_dict[shelter_name] = [shelter_type, shelter_lat, shelter_lon, shelter_capacity,
+                                          shelter_occupancy, food_available, water_available,
+                                          medical_aid_available, pet_friendly]
+        return shelter_dict
 
 
+def main():
+    shelter_dict = read_csv_to_dict("shelters.csv")
 
+
+main()
